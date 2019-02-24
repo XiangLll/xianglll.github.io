895f1009f4c5e3f7661ed41df6ab7adc1d17eaa0
│･ω･`) <shining.happyluckygo@gmail.com>
2019-02-24 10:41:08 -0700

    Adding special thanks and code snippet reference
    Thank you so much for my mentor and the original author of the code snippets being referenced. We looked up so many pages for the count-down animation and got inspirations from so many of them. I apologize for not listing up every single page that we saw on here, but I truly appreciate all the authors who selflessly opened up their own code to the internet and let many benefited from learning from their examples. Lots lots of thanks!

--- timer.js
+++ timer.js
@@ -1,3 +1,9 @@
+// Great thanks to my mentor: Lauren Raddatz , who had spent hours figuring out the timer effect and improving on page layout with me.
+// This count down effect was modified based on the code snippet found at here:
+// https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
+// We tried to make the "Ready?" on the 1st page being part of the count down, but that turned out to be way too complicated so we decided to move the "Ready?" to the 1st page and let it be the link that the user clicks on and leads to the second page.
+
+
 var timeleft = 3;
 var downloadTimer = setInterval(function(){
   document.getElementById("seconds").innerHTML = timeleft;
